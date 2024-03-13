8e41b5964ac84017af1ae5ae8e48dc191067749c
devgpt-ai <endaqi@jiewatech.com>
2024-03-13 08:10:08 +0000

    Task description

--- go/testDebug.go
+++ go/testDebug.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3}
-	fmt.Println(slice[3]) // 这里访问了超出切片范围的索引，会导致 panic
+slice := []int{1, 2, 3}
+if len(slice) > 3 {
+    fmt.Println(slice[3])
+} else {
+    fmt.Println("Index out of range")
+}
+ // 这里访问了超出切片范围的索引，会导致 panic
 }
